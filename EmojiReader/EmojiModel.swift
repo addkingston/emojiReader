9f1db45b8dc83a2ac0bc8874afe91b1ab6c568e1
Faww <weedsm@mail.ru>
2022-03-10 15:29:06 +0300

    10.03

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by MacBook on 09.03.2022.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
